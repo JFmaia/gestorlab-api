1c40f62d0e46b128237ad2c52fd084d28cd87948
JFmaia <jfmaia.dev@gmail.com>
2024-04-09 19:50:41 -0300

    feat: Creating a new session for the test database (#16)

--- core/database.py
+++ core/database.py
@@ -1,13 +1,23 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
 from core.config import settings
+from core.config_test import settingsTest
 
 engine: AsyncEngine = create_async_engine(settings.DB_URL)
+enginetest: AsyncEngine = create_async_engine(settingsTest.DB_URL_TEST)
 
-Session: AsyncSession = sessionmaker(
+Session: sessionmaker = sessionmaker(
     autoflush=False,
     autocommit=False,
     expire_on_commit=False,
     class_= AsyncSession,
     bind=engine
+)
+
+SessionTest: sessionmaker = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+    class_= AsyncSession,
+    bind=enginetest
 )
