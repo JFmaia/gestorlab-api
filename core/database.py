a1737a2f75e8482146a8d73f96ddc1f4b605d7cc
JFmaia <jfmaia.dev@gmail.com>
2024-04-11 20:44:11 -0300

    feat: Adjusting database.py (#16)

--- core/database.py
+++ core/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
 from core.config import settings
-from core.config_test import settingsTest
+import os
 
 engine: AsyncEngine = create_async_engine(settings.DB_URL)
-enginetest: AsyncEngine = create_async_engine(settingsTest.DB_URL_TEST)
 
 Session: sessionmaker = sessionmaker(
     autoflush=False,
@@ -12,12 +11,4 @@ Session: sessionmaker = sessionmaker(
     expire_on_commit=False,
     class_= AsyncSession,
     bind=engine
-)
-
-SessionTest: sessionmaker = sessionmaker(
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=False,
-    class_= AsyncSession,
-    bind=enginetest
 )
