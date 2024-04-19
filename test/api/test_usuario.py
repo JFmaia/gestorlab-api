4816bfed97dac8deec840aa0c24991a0af6b37e3
JFmaia <jfmaia.dev@gmail.com>
2024-04-18 22:37:24 -0300

    feat: Create new logic test (#16)

--- test/api/test_usuario.py
+++ test/api/test_usuario.py
@@ -1,4 +1,5 @@
 from test.utils.utils_usuario import create_user_valido
+
 def test_cria_usuario(client):
     body = create_user_valido()
     response = client.post('/gestorlab/usuarios/signup', json=body)
--- test/conftest.py
+++ test/conftest.py
@@ -1,32 +1,55 @@
-import pytest
 from fastapi.testclient import TestClient
-from core.config_test import settingsTest
-from core.database import enginetest
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from core.config import settings
+from typing import AsyncGenerator
 from main import app
-import os
+import pytest
+from core.deps import get_session
 
-@pytest.fixture(scope="session", autouse=True)
-async def setup_and_teardown():
-    # Setup: set TESTING environment variable to True
-    os.environ["TESTING"] = "True"
-    
-    # Create tables for the test database
-    async with enginetest.begin() as conn:
-        await conn.run_sync(settingsTest.DBBaseModel.metadata.create_all)
-    
-    # Run the tests
-    yield
-    
-    # Teardown: set TESTING environment variable to False
-    os.environ["TESTING"] = "False"
-    
-    # Drop all tables after the tests are done
-    async with enginetest.begin() as conn:
-        await conn.run_sync(settingsTest.DBBaseModel.metadata.drop_all)
-        
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test.db"  # Use o prefixo 'sqlite+aiosqlite://' para indicar o uso do aiosqlite
+
+# Crie um AsyncEngine usando o aiosqlite
+engine: AsyncEngine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+# Crie uma função de fábrica de sessão assíncrona
+TestingSessionLocal: sessionmaker = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    bind=engine
+)
+
+
+async def override_get_db() -> AsyncGenerator:
+    async with TestingSessionLocal() as session:
+        yield session
+
+
+app.dependency_overrides[get_session] = override_get_db
+
 
 @pytest.fixture
 def client():
     with TestClient(app) as client:
         yield client
 
+
+# Crie todas as tabelas manualmente usando 'async with engine.begin()'
+@pytest.fixture(autouse=True)
+async def create_test_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+
+
+# Remova todas as tabelas após os testes
+@pytest.fixture(autouse=True)
+async def drop_test_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
