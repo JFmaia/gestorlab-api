15362f8bac1dc492b9ecf178e249e3e1c378ad62
JFmaia <jfmaia.dev@gmail.com>
2024-04-18 22:34:14 -0300

    feat: Adjusting in models, modified type UUID (#16)

--- models/associetions.py
+++ models/associetions.py
@@ -1,19 +1,20 @@
 from sqlalchemy import Column, ForeignKey, Table
 from core.config import settings
+from sqlalchemy_utils import UUIDType
 from sqlalchemy.dialects.postgresql import UUID
 
 # Tabela de associação muitos para muitos entre Usuario e Laboratorio
 usuario_laboratorio_association = Table(
     'usuario_laboratorio',
     settings.DBBaseModel.metadata,
-    Column('usuario_id', UUID(as_uuid=True), ForeignKey('usuario.id')),
-    Column('laboratorio_id', UUID(as_uuid=True), ForeignKey('laboratorios.id'))
+    Column('usuario_id', UUIDType(binary=False), ForeignKey('usuario.id')),
+    Column('laboratorio_id', UUIDType(binary=False), ForeignKey('laboratorios.id'))
 )
 
 # Tabela de associação muitos para muitos entre Usuario e Projeto
 usuario_projeto_association = Table(
     'usuario_projeto',
     settings.DBBaseModel.metadata,
-    Column('usuario_id', UUID(as_uuid=True), ForeignKey('usuario.id')),
-    Column('projeto_id', UUID(as_uuid=True), ForeignKey('projetos.id'))
+    Column('usuario_id', UUIDType(binary=False), ForeignKey('usuario.id')),
+    Column('projeto_id', UUIDType(binary=False), ForeignKey('projetos.id'))
 )
--- models/projeto.py
+++ models/projeto.py
@@ -3,15 +3,15 @@ from datetime import datetime
 from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from core.config import settings
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import UUIDType
 from models.associetions import usuario_projeto_association
 class Projeto(settings.DBBaseModel):
     __tablename__ = 'projetos'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
     titulo = Column(String(256), unique=True, nullable=False)
     descricao = Column(String(5000), nullable=True)
-    autor_id = Column(UUID(as_uuid=True), ForeignKey("usuario.id"), nullable=False)
+    autor_id = Column(UUIDType(binary=False), ForeignKey("usuario.id"), nullable=False)
     data_inicial = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
     data_up = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
     autor = relationship("Usuario", back_populates='projetos', lazy='joined')
