2a30ae640e59988c621b3b0cd50849dab639afde
JFmaia <jfmaia.dev@gmail.com>
2024-06-26 19:02:30 -0300

    feat: Create new model permissao (#19)

---
+++ models/permissao.py
@@ -0,0 +1,10 @@
+import uuid
+from sqlalchemy import Column, String
+from core.config import settings
+from sqlalchemy_utils import UUIDType
+
+class Permissao(settings.DBBaseModel):
+  __tablename__ = 'permissao'
+
+  id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
+  title = Column(String(256), nullable=False)
