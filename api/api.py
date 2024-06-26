4b85ae7e0c908dde4b53e6ebac2238db1ed4c3fd
JFmaia <jfmaia.dev@gmail.com>
2024-06-26 20:05:58 -0300

    feat: Create nnew router (#19)

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from api.endpoints import usuario, laboratorio, projeto
+from api.endpoints import usuario, laboratorio, projeto, permissao
 
 api_router = APIRouter()
 
 api_router.include_router(usuario.router, prefix='/usuarios', tags=["usuarios"])
 api_router.include_router(laboratorio.router, prefix='/laboratorios' ,tags=["laboratorios"])
 api_router.include_router(projeto.router, prefix='/projetos' ,tags=["projetos"])
+api_router.include_router(permissao.router, prefix='/permissoes' ,tags=["permissoes"])
