0307f4f7a5d758fdac773f7b6827c3137a2ed9a5
JFmaia <jfmaia.dev@gmail.com>
2024-06-26 19:00:12 -0300

    feat: Create new associetion (#19)

--- models/associetions.py
+++ models/associetions.py
@@ -24,3 +24,10 @@ laboratorio_projeto_association = Table(
     Column('laboratorio_id', UUIDType(binary=False), ForeignKey('laboratorios.id')),
     Column('projeto_id', UUIDType(binary=False), ForeignKey('projetos.id'))
 )
+
+usuario_permission_association = Table(
+    'usuario_permissao',
+    settings.DBBaseModel.metadata,
+    Column('usuario_id', UUIDType(binary=False), ForeignKey('usuario.id')),
+    Column('permissao_id', UUIDType(binary=False), ForeignKey('permissao.id'))
+)
