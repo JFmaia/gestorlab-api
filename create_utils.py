00eadb1029b53d6f0a01798d1499ff519948446a
JFmaia <jfmaia.dev@gmail.com>
2024-06-27 16:06:53 -0300

    feat: New function for create generos (#19)

--- create_utils.py
+++ create_utils.py
@@ -1,5 +1,6 @@
 from core.database import Session
 from models.permissao import Permissao
+from models.genero import Genero
 
 
 def create_permissions():
@@ -9,9 +10,9 @@ def create_permissions():
     session = Session()
     # Defina os nomes das permissões que deseja criar
     permissions = [
-        "admin",
-        "coordenador",
-        "membro"
+        "Admin",
+        "Coordenador",
+        "Membro"
     ]
 
     # Verifique se alguma permissão já existe antes de criar
@@ -32,8 +33,41 @@ def create_permissions():
     # Feche a sessão
     session.close()
 
+def create_generos():
+    """Função para criar quatro permissões na tabela de permissão"""
+
+    # Crie uma sessão do banco de dados
+    session = Session()
+    # Defina os nomes das permissões que deseja criar
+    generos = [
+        "Masculino", 
+        "Feminino", 
+        "Transgênero", 
+        "Gênero neutro", 
+        "Não-binário"
+    ]
+
+    # Verifique se alguma permissão já existe antes de criar
+    existing_generos = session.query(Genero).filter(Genero.title.in_(generos)).all()
+
+    # Filtre as permissões que não existem ainda
+    genero_to_create = [gene for gene in generos if gene not in [p.title for p in existing_generos]]
+
+    # Crie objetos Permissão para cada permissão que precisa ser criada
+    new_genero = [Genero(title=gene) for gene in genero_to_create]
+
+    # Adicione as novas permissões à sessão
+    session.add_all(new_genero)
+
+    # Confirme as alterações no banco de dados
+    session.commit()
+
+    # Feche a sessão
+    session.close()
 
 # Verifique se o script está sendo executado diretamente
 if __name__ == "__main__":
     create_permissions()
     print("Permissões criadas com sucesso!")
+    create_generos()
+    print("Generos criados com sucesso!")
