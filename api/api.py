bf8129ab1bfbe0dcf7dcff02af01275d9e5dde0e
JFmaia <jfmaia.dev@gmail.com>
2024-06-27 16:02:04 -0300

    feat: Add new router genero (#19)

--- api/api.py
+++ api/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.endpoints import usuario, laboratorio, projeto, permissao
+from api.endpoints import usuario, laboratorio, projeto, permissao, genero
 
 api_router = APIRouter()
 
@@ -7,3 +7,4 @@ api_router.include_router(usuario.router, prefix='/usuarios', tags=["usuarios"])
 api_router.include_router(laboratorio.router, prefix='/laboratorios' ,tags=["laboratorios"])
 api_router.include_router(projeto.router, prefix='/projetos' ,tags=["projetos"])
 api_router.include_router(permissao.router, prefix='/permissoes' ,tags=["permissoes"])
+api_router.include_router(genero.router, prefix='/generos' ,tags=["generos"])
