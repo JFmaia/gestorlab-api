64645a4374872ff60b329a3a79d3c2747f7f8e7d
JFmaia <jfmaia.dev@gmail.com>
2024-07-23 18:26:27 -0300

    feat: Create new endpoint in permissao (#40)

--- api/endpoints/permissao.py
+++ api/endpoints/permissao.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, status, Depends
 from sqlalchemy.future import  select
 from sqlalchemy.orm import Session
 from models.permissao import Permissao
+from models.permissaoLab import PermissaoOfLab
 from schemas.permissao_schema import PermissaoSchema
 from core.deps import get_session, get_current_user
 
@@ -15,5 +16,14 @@ async def get_permissoes(db: Session = Depends(get_session)):
   query = select(Permissao)
   result = db.execute(query)
   permissaos: List[Permissao] = result.scalars().unique().all()
+
+  return permissaos
+
+#GET Permissões
+@router.get('/permLab', response_model= List[PermissaoSchema], status_code=status.HTTP_200_OK)
+async def get_permissoes(db: Session = Depends(get_session)):
+  query = select(PermissaoOfLab)
+  result = db.execute(query)
+  permissaos: List[PermissaoOfLab] = result.scalars().unique().all()
 
   return permissaos
