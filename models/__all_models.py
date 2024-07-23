6af7d9fee979eb218437aa0fad7bc843f3dca56a
JFmaia <jfmaia.dev@gmail.com>
2024-07-23 18:25:41 -0300

    feat: Add new model (#40)

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,6 +1,7 @@
 from models.laboratorio import Laboratorio
 from models.projeto import Projeto
 from models.permissao import Permissao
+from models.permissaoLab import PermissaoOfLab
 from models.genero import Genero
 from models.pending import Pending
 from models.usuario import Usuario
