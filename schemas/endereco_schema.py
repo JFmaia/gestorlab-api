97d71578e82b3e4502f18a2ce4083a64bd49f20d
JFmaia <jfmaia.dev@gmail.com>
2024-08-12 09:26:53 -0300

    feat: Add create new schema (#40)

---
+++ schemas/endereco_schema.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, UUID4
+
+class EnderecoSchema(BaseModel):
+  id: Optional[UUID4] = None
+  logradouro: str
+  numero:int
+  complemento:str
+  bairro:str
+  cidade:str
+  estado:str
+  cep:int
+  pais:str
+  data_inicial:Optional[datetime] = None
+  data_up:Optional[datetime] = None
+
+  class Config:
+      from_attributes = True
