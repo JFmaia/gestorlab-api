76cb8a9879397b772b116848bed327db545d0d65
JFmaia <jfmaia.dev@gmail.com>
2024-08-13 17:08:42 -0300

    feat: Modified model pending (#40)

--- models/pending.py
+++ models/pending.py
@@ -8,9 +8,12 @@ class Pending(settings.DBBaseModel):
   __tablename__ = 'pendings'
 
   id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
-  id_user = Column(UUIDType(binary=False), nullable=True) 
+  id_user = Column(UUIDType(binary=False), nullable=True)
+  nome_user = Column(String(256), nullable=False) 
   id_lab = Column(UUIDType(binary=False), nullable=True)
+  nome_lab = Column(String(256), nullable=False)
   id_project = Column(UUIDType(binary=False), nullable=True)
+  nome_project = Column(String(256), nullable=False)
   ativo = Column(Boolean(True),nullable=False, default=True)
   data_create = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
   data_atualizacao = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
